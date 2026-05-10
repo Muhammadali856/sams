ba1a8bbadd6964a635a2a17a21db2caee5ca9f09
Muhammadali <eshboevmuhammadali2@gmail.com>
2026-05-10 11:53:35 +0800

    update registerstudentserializer

--- api/serializers.py
+++ api/serializers.py
@@ -34,21 +34,25 @@ class StudentSerializer(serializers.ModelSerializer):
 class RegisterStudentSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
     programme_id = serializers.IntegerField(write_only=True)
+    
+    # This line tells Django: "Look for 'student_id' in the JSON, 
+    # but save it into the 'username' column in the database."
+    student_id = serializers.CharField(source='username') 
 
     class Meta:
         model = User
-        fields = ['username', 'password', 'first_name', 'last_name', 'email', 'programme_id']
+        # IMPORTANT: Remove 'username' and add 'student_id' here
+        fields = ['student_id', 'password', 'first_name', 'last_name', 'email', 'programme_id']
 
     def create(self, validated_data):
         password = validated_data.pop('password')
         programme_id = validated_data.pop('programme_id')
         
-        # 1. Asosiy User'ni yaratamiz va parolni shifrlaymiz
+        # validated_data['username'] will now contain what the frontend sent as 'student_id'
         user = User.objects.create(**validated_data)
         user.set_password(password)
         user.save()
 
-        # 2. Ushbu user uchun Student profilini yaratamiz va yo'nalishni biriktiramiz
         programme = Programme.objects.get(id=programme_id)
         Student.objects.create(user=user, programme=programme)
 
