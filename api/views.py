608dceb769b10354721192cef069f8ad604fc921
Muhammadali <eshboevmuhammadali2@gmail.com>
2026-05-11 16:56:50 +0800

    added API to see students 1.1

--- api/views.py
+++ api/views.py
@@ -3,7 +3,7 @@ from rest_framework.response import Response
 from rest_framework import permissions
 from rest_framework.permissions import IsAuthenticated
 from .models import Programme, Assignment, Task, Student, Teacher
-from .serializers import AssignmentSerializer, TaskSerializer, 
+from .serializers import AssignmentSerializer, TaskSerializer
 from .serializers import StudentSerializer, RegisterStudentSerializer, ProgrammeSerializer
 
 class IsTeacherOrReadOnly(permissions.BasePermission):
