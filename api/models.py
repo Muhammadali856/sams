f12b8c772c85a9b01712d823ef8478cffe9612a3
Muhammadali <eshboevmuhammadali2@gmail.com>
2026-05-11 19:08:57 +0800

    Changed programme model

--- api/models.py
+++ api/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 # 1. Programmes (Dasturlar) jadvali
 class Programme(models.Model):
     name = models.CharField(max_length=255, unique=True)
+    description = models.TextField(null=True, blank=True)
     
     def __str__(self):
         return self.name
