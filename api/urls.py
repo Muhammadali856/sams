3870a9669eb379d3e61931f3520f187a8bcb7ce7
Muhammadali <eshboevmuhammadali2@gmail.com>
2026-05-12 17:01:21 +0800

    added update programmes for students 1.1

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import AssignmentViewSet, TaskViewSet, StudentSignUpView, ProgrammeViewSet, StudentViewSet
+from .views import AssignmentViewSet, TaskViewSet, StudentSignUpView 
+from .views import UpdateStudentProgrammesView, ProgrammeViewSet, StudentViewSet
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 router = DefaultRouter()
