5bef2d22c11bcb484c06ee74757166c1e46e6152
Muhammadali <eshboevmuhammadali2@gmail.com>
2026-05-12 18:04:17 +0800

    added quizzes in admin page 1.1

--- api/admin.py
+++ api/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Programme, Teacher, Student, Assignment, Task
+from .models import Programme, Teacher, Student, Assignment, Task, Quiz
 
 @admin.register(Programme)
 class ProgrammeAdmin(admin.ModelAdmin):
